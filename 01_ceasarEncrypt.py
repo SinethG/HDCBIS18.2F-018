cffb92e81a0baf74c6b0d0ecfd3ac51ca2e61251
SinethG <44729995+SinethG@users.noreply.github.com>
2018-11-03 21:51:49 +0530

    Add files via upload

---
+++ 01_ceasarEncrypt.py
@@ -0,0 +1,8 @@
+text=input("Input Text:")
+alphabet="abcdefghijklmnopqrstuvwxyz"
+k=3
+cipher=""
+for c in text:
+  if c in alphabet:
+    cipher +=alphabet[(alphabet.index(c)+k)%(len(alphabet))]
+print('Your Encrypted Message is '+ cipher) 
